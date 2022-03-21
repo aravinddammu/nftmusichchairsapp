21821868ee71196b1053e94fb5fd6ac075f3dff6
aravinddammu <aravind@solulab.com>
2022-03-21 11:46:38 +0530

    account retreived

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useMetamask } from 'use-metamask';
+import { ethers } from 'ethers';
 
 interface Props {
   name: String;
@@ -12,6 +14,30 @@ interface nft {
 
 export default function TestComponent<T extends nft>(props: Props) {
   const [nfts, setNfts] = useState<nft[]>([]);
+  const {connect, getAccounts, metaState} = useMetamask();
+  const [web3interface, setWeb3interface] = useState("ethers");
+
+  useEffect(() => {
+    if (metaState.isAvailable && !metaState.isConnected) {
+      (async () => {
+        try {
+          if (web3interface === "ethers")
+            await connect(ethers.providers.Web3Provider, "any");
+          else 
+            throw Error(`Unknown web3 interface: ${web3interface}`);
+        } catch (error) {
+          console.log(error);
+        }
+      })
+      
+      ();
+    }
+  }, [metaState.isAvailable, web3interface]);
+
+  useEffect(()=>{
+    const account = getAccounts();
+    console.log("account: ", account);
+  },[metaState.isConnected])
 
   useEffect(() => {
     axios.get("http://localhost:3000/nfts")
@@ -21,18 +47,22 @@ export default function TestComponent<T extends nft>(props: Props) {
     })
     .catch(error=>{
       console.log(error);
-    })
-    ;
+    });
+
   }, []);
+
+  const handleForward = () : void => {
+    console.log("test");
+  }
   return (
     <div>
       <h2>{props.name}</h2>
       {nfts.map(nft=>{
         return (<div style={{backgroundColor: "whitesmoke", width: "30%", marginLeft: "30px"}}>
-          <h4 key={nft?.id}>
+          <h4 key={nft.id}>
             {nft?.name}
           </h4>
-          <button>Forward NFT</button>
+          <button onClick={handleForward}>Forward NFT</button>
           </div>);
       })}
     </div>
